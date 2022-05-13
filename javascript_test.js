bfb11c5359e7fb67be0ca897153ccedf2e62d1c9
wiskky <adeniyi.wisdom@gmail.com>
2022-05-13 14:13:17 +0100

    update the file

---
+++ javascript_test.js
@@ -0,0 +1,84 @@
+/*
+console.log("Hello world");
+var myString = 'What is the meaning of life?';
+var myNo = 42;
+myBool = true;
+var myArray = [myString, myNo, myBool];
+console.log("this is my array list " + '(' + myArray +')' );
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+*/
+
+/* How to create an array in JS
+var myArray = new Array(1,2,3,4)
+var myArray = [1,2,3,4]
+Both are the same
+ */
+
+var myStack = [];
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+myStack.push(4);
+myStack.push(5);
+myStack.push(6);
+myStack.push(7);
+
+//myStack.pop(3);
+//console.log(myStack);
+//console.log(myStack.shift());
+//console.log(myStack.unshift());
+//myStack.unshift()
+//myStack.shift()
+
+//Splicing arrays in JavaScript removes a certain part from 
+//an array to create a new array, made up from the part we take out.
+//var splice = myStack.splice(2,7);
+//console.log(splice)
+//console.log(myStack)
+
+/*
+console.log(Math.pow(2,3))
+
+JavaScript also has a Math module which contains more advanced functions:
+
+Math.abs calculates the absolute value of a number
+Math.exp calculates e to the power of a number
+Math.pow(x,y) calculates the result of x to the power of y
+Math.floor removes the fraction part from a number
+Math.random() will give a random number x where 0<=x<1
+And many more mathematical functions.
+*/
+
+/*
+//The if statement allows us to check if an expression is equal to true or 
+//false, and execute different code according to the result.
+if (condition){
+    code here
+}
+//To evaluate whether two variables are equal, the == operator is used. 
+//There is also another equality operator in JavaScript, ===, 
+//which does a strict comparison.
+
+*/
+
+/*
+//The switch statement is used to select between more than two different 
+//options, and to run the same code for more than one option
+
+switch(condition){
+    case 1:
+        statement here;
+        break;
+    case 2:
+        statement here;
+        break;
+    :
+    :
+    default:
+        statement here;
+        break; 
+
+}
+*/
